a6ab07df41e7d93241a13e03d754f11bd2d47867
unknown <mtbutt@deakin.edu.au>
2022-02-05 21:21:51 +1100

    delete movies

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -4,7 +4,47 @@ const backdrop = document.getElementById('backdrop');
 const cancelAddMovieButton = addMovieModal.querySelector('.btn--passive');
 const addMovieButton = cancelAddMovieButton.nextElementSibling;
 const userInput = addMovieModal.querySelectorAll('input');
+const textSection = document.getElementById('entry-text');
+const listRoot = document.getElementById('movie-list');
 const Movies = [];
+
+const deleteElement = (movieID) => {
+    let movieIndex = 0;
+    for (const movie of Movies){
+        if (movie.id == movieID){
+            break;
+        }
+        movieIndex++;
+    }
+    Movies.slice(movieIndex, 1);
+    listRoot.children[movieIndex].remove();
+};
+
+const renderNewMovieElement = (id, title, imageURL, rating) => {
+    const newMovieElement = document.createElement('li');
+    newMovieElement.className = 'movie-element';
+    newMovieElement.innerHTML = `
+    <div class="movie-element__image">
+        <img src="${imageURL}" alt="${title}">
+    </div>
+    <div class="movie-element__info">
+        <h2>${title}</h2>
+        <h2>${rating}/5 stars</h2>
+    </div>
+    `;
+    
+    listRoot.append(newMovieElement);
+    newMovieElement.addEventListener('click', deleteElement.bind(null, id));
+
+};
+
+const clearTextSection = () => {
+    if (Movies.length === 0){
+        textSection.style.display = 'block'
+    }else {
+        textSection.style.display = 'none';
+    }
+};
 
 const toggleMovieModal = () => {
     addMovieModal.classList.toggle('visible');
@@ -42,6 +82,7 @@ const addButtonHandler = () => {
     }
 
     const newMovies = {
+        id: Math.random().toString(),
         title: userTitle,
         image: userIamgeURL,
         rating: userRating
@@ -50,6 +91,8 @@ const addButtonHandler = () => {
     Movies.push(newMovies);
     console.log(Movies);
     toggleMovieModal();
+    clearTextSection();
+    renderNewMovieElement(newMovies.id, newMovies.title, newMovies.image, newMovies.rating);
     clearInputs();
 };
 
